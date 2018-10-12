14514f1e4eb29f3ec901131e6b01190accd53a41
riskah14 <riskah1412@gmail.com>
2018-10-12 09:24:19 +0700

    first commit

---
+++ KaryawanTest.java
@@ -0,0 +1,19 @@
+public class KaryawanTest{
+	public static void main(String[] args) {
+		Desain d = new Desain();
+		d.setNama("riskah");
+		d.setAlamat("rawadalem");
+		d.setGaji(90);
+
+		Programmer p = new Programmer();
+		p.setNama ("chusnul");
+		p.setAlamat("kertasmaya");
+		p.setGaji(80);
+
+		PM c = new PM();
+		c.setNama("shely");
+		c.setAlamat("cirebon");
+		c.setGaji(60);
+
+	}
+}
